5be552120b73f654a42cb0aa6843c3fd09db9bc8
Felipe <pipechg@hotmail.com>
2013-08-03 16:33:41 -0500

    Primer ejercicio a entregar.

---
+++ IdeaProjects/taller6/src/test/java/co/edu/um/taller6/OperacionesMatematicasTest.java
@@ -0,0 +1,68 @@
+package co.edu.um.taller6;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+/**
+ * Realiza las pruebas unitarias para la clase OperacionesMatematicas
+ */
+public class OperacionesMatematicasTest {
+    @Test
+    public void testSuma() throws Exception {
+
+        OperacionesMatematicas op = new OperacionesMatematicas();
+
+        Assert.assertNotNull(op);
+
+        Assert.assertEquals(5,op.suma(3,2));
+
+        Assert.assertEquals(-3,op.suma(-2,-1));
+
+        Assert.assertEquals(0,op.suma(0,0));
+
+
+    }
+
+    @Test
+    public void testResta() throws Exception {
+
+        OperacionesMatematicas op = new OperacionesMatematicas();
+
+        Assert.assertNotNull(op);
+
+        Assert.assertEquals(1,op.resta(3,2));
+
+        Assert.assertEquals(-1,op.resta(-2,-1));
+
+        Assert.assertEquals(0,op.resta(0,0));
+
+    }
+
+    @Test
+    public void testMultiplicacion() throws Exception {
+
+        OperacionesMatematicas op = new OperacionesMatematicas();
+
+        Assert.assertNotNull(op);
+
+        Assert.assertEquals(6,op.multiplicacion(3,2));
+
+        Assert.assertEquals(2,op.multiplicacion(-2,-1));
+
+        Assert.assertEquals(0,op.multiplicacion(2,0));
+
+    }
+
+    @Test(expected = ArithmeticException.class)   //En esta parte se coloca la expecion
+
+    public void testDivision() throws Exception {
+        OperacionesMatematicas op = new OperacionesMatematicas();
+
+        Assert.assertNotNull(op);
+
+        // Assert.assertEquals(2,op.division(4,2));  Esta es la exeepcion
+
+        op.division(2,0);
+
+    }
+}
